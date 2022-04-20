588d3e1208677cf67a6ff46d8096a34d423e0db7
BrendonW250 <brendonwright16@gmail.com>
2022-04-19 22:42:05 -0400

    8ky

--- 8-toSquareOrNot.js
+++ 8-toSquareOrNot.js
@@ -1,14 +1,46 @@
 
+// Write a method, that will get an integer array as 
+// parameter and will process every number from this array.
 
-// solution 
+// Return a new array with 
+// processing every number of the input-array like this:
+// If the number has an integer 
+// square root, take this, otherwise square the number.
+
+// Repitition
 function squareOrSquareRoot(arr){
     let newArr = []
-    for(let i = 0; i < arr.length; i++){
-        if (Math.sqrt(arr[i] % 1 === 0)){
+    for (let i = 0; i < arr.length; i++){
+        if (Math.sqrt(arr[i]) % 1 === 0){
             newArr.push(Math.sqrt(arr[i]))
         }else{
             newArr.push(Math.pow(arr[i], 2))
         }
     }
-    return newArr
+    return newArr 
 }
+
+// function squareOrSquareRoot(arr){
+//     let newArr = []
+//     for (let i = 0; i < arr.length; i++){
+//         if (Math.sqrt(arr[i] % 1 === 0)){
+//             newArr.push(Math.sqrt(arr[i]))
+//         }else {
+//             newArr.push(Math.pow(arr[i], 2))
+//         }
+//     }
+//     return newArr
+// }
+
+// solution 
+// function squareOrSquareRoot(arr){
+//     let newArr = []
+//     for(let i = 0; i < arr.length; i++){
+//         if (Math.sqrt(arr[i] % 1 === 0)){
+//             newArr.push(Math.sqrt(arr[i]))
+//         }else{
+//             newArr.push(Math.pow(arr[i], 2))
+//         }
+//     }
+//     return newArr
+// }
