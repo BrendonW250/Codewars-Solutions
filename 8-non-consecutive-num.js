2903265136d7009be26004d97e3ce7cd3ef42cd5
BrendonW250 <46827649+BrendonW250@users.noreply.github.com>
2022-04-24 23:46:31 -0400

    Update 8-non-consecutive-num.js

--- 8-non-consecutive-num.js
+++ 8-non-consecutive-num.js
@@ -3,7 +3,7 @@
 
 
 function nonConsecutiveNum(arr){
-    for (let i=0; i<arr.length; i++){
+    for (let i=0; i<arr.length-1; i++){
         if (arr[i]+1 !== arr[i+1]){
             return arr[i+1]
         }
