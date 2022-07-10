b7ba4498a4cd989e0c6be3d4b0f9e385bc5804f7
BrendonW250 <brendonwright16@gmail.com>
2022-07-09 23:23:27 -0400

    7kyu

---
+++ 7kyu-katas/7-fizzBuzz.js
@@ -0,0 +1,36 @@
+// Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value "Fizz" instead
+// If the value is a multiple of 5: use the value "Buzz" instead
+// If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+// N will never be less than 1.
+
+// Method calling example:
+
+// fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+// Return an array
+function fizzbuzz(n)
+{
+  //
+  let arr = []
+  
+//   putting every number between 1 & 10 in arr
+  for (let i=1; i<=n; i++){
+    arr.push(i)
+  }
+  
+//   going through arr for expected outcomes
+  for (let i=0; i<arr.length; i++){
+    if (arr[i] % 3 == 0 && arr[i] % 5 == 0){
+      arr.splice(i, 1, "FizzBuzz")
+    }else if (arr[i] % 5 == 0){
+      arr.splice(i, 1, 'Buzz')
+    }else if (arr[i] % 3 == 0){
+      arr.splice(i, 1, 'Fizz')
+    }
+  }
+  return arr
+}
