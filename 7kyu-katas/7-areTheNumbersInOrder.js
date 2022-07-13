b3c95f6da773e3114976baf260fcf60544f34da5
BrendonW250 <brendonwright16@gmail.com>
2022-07-12 23:56:17 -0400

    7kyu

--- 7kyu-katas/7-areTheNumbersInOrder.js
+++ 7kyu-katas/7-areTheNumbersInOrder.js
@@ -1,6 +1,6 @@
 
 // Are the numbers in order?
-// In this Kata, your function receives an array of integers as
+// In this Kata, your function receives an array of integers as 
 //  input. Your task is to determine whether the numbers are in 
 //  ascending order. An array is said to be in ascending order if 
 //  there are no two adjacent integers where the left integer exceeds the right integer in value.
