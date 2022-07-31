c661f39756c0ad48326b6be1aa8668480feb9af2
BrendonW250 <brendonwright16@gmail.com>
2022-07-30 23:54:54 -0400

    7kyu

---
+++ 7kyu-katas/7-sortArrays1.js
@@ -0,0 +1,9 @@
+// Just a simple sorting usage. Create a function that 
+// returns the elements of the input-array / list 
+// sorted in lexicographical order.
+
+function sortme(names){
+    let sorted = names.sort()
+
+    return sorted
+}
