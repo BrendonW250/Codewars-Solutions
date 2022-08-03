b3797ccd1806169a3076173b5a14b8f57c54d25a
BrendonW250 <brendonwright16@gmail.com>
2022-08-02 22:46:02 -0400

    7kyu

---
+++ 7kyu-katas/7-bumpsInTheRoad.js
@@ -0,0 +1,24 @@
+// Your car is old, it breaks easily. The shock absorbers are gone and 
+// you think it can handle about 15 more bumps before it dies totally.
+
+// Unfortunately for you, your drive is very bumpy! Given a string showing 
+// either flat road (_) or bumps (n). If you are able to reach home safely 
+// by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+
+function bump(x){
+    let count = 0
+    
+    // loop through the string and 
+    // for any 'n' found increase the count by 1
+    for (let i=0; i<x.length; i++){
+      if (x[i] === 'n'){
+        count++
+      }
+    }
+    // if count is over 15 return 'Car Dead'
+    if (count > 15){
+      return 'Car Dead'
+    }else if (count <= 15){ //else return 'woohoo!'
+      return 'Woohoo!'
+    }
+  }
